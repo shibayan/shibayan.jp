e272bafc8f332af72ba1548bd49ee70db9e92469
Tatsuro Shibamura <me@shibayan.jp>
2020-05-07 01:07:41 +0900

    Adding GitHub link

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -7,5 +7,10 @@ module.exports = {
         'ga': 'UA-772309-13'
       }
     ]
-  ]
+  ],
+  themeConfig: {
+    nav: [
+      { text: 'GitHub', link: 'https://github.com/shibayan/shibayan.jp' }
+    ]
+  }
 }

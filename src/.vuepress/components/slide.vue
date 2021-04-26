cfa33b9fd7535675dfaf7d427328345f6e7a62e6
Tatsuro Shibamura <me@shibayan.jp>
2021-04-26 16:00:24 +0900

    Adding slide index page

---
+++ src/.vuepress/components/slide.vue
@@ -0,0 +1,17 @@
+<style scoped>
+  iframe {
+    margin: 1.0em 0;
+  }
+</style>
+<template>
+  <iframe :src="src" width="720" height="405" frameborder="0" allowfullscreen></iframe>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      src: ""
+    };
+  }
+};
+</script>
---
+++ src/slides/index.md
@@ -0,0 +1,19 @@
+---
+title: 'Slides'
+---
+
+## 2021
+
+### Cosmos DB Conf
+
+<slide src="./cosmosdbconf" />
+
+### Hack Azure! #5
+
+<slide src="./hackazure5" />
+
+## 2020
+
+### .NET Conf Online
+
+<slide src="./dotnetconf" />
